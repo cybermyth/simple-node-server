e82950783e81dd6bd06617176b18723a713305af
cybermyth <matej.fico@gmail.com>
2018-08-11 20:49:37 +0200

    Added prepareResponse function

--- server.js
+++ server.js
@@ -5,27 +5,32 @@ const app = express();
 const version = process.env.VERSION || 'v1';
 const started = new Date();
 
+function prepareResponse() {
+  const timestamp = new Date();
+  const hostname = os.hostname();
+  
+  const response = {
+      hostname,
+      version,
+      timestamp
+  };
+  return response;
+}
+
 app.get('/', (req, res) => {
-    const timestamp = new Date();
-    const hostname = os.hostname();
-    
-    const response = {
-        hostname,
-        version,
-        timestamp
-    };
-    console.log(`${timestamp}: Running version ${version} on ${hostname}`);
-    res.send(response);
+  const response = prepareResponse();
+  console.log(`${response.timestamp}: Running version ${response.version} on ${response.hostname}`);
+  res.send(response);
 });
 
 app.get('/healthz', (req, res) => {
-    const runningTime = Math.abs((new Date().getTime() - started.getTime()) / 1000);
-    console.log(runningTime);
-    if (runningTime <= 15) {
-        res.status(500).send(`Error occured: ${runningTime}`);
-    } else {
-        res.status(200).send(`Call was successful: ${runningTime}`)
-    }
+  const runningTime = Math.abs((new Date().getTime() - started.getTime()) / 1000);
+  console.log(runningTime);
+  if (runningTime <= 15) {
+    res.status(500).send(`Error occured: ${runningTime}`);
+  } else {
+    res.status(200).send(`Call was successful: ${runningTime}`)
+  }
 });
 
 app.listen(4000, () => console.log('Server listening on 4000'));
